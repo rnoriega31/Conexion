0a6a6140fa8ace3ddc806701aa8ef301d0cacfa3
Ricardo <r.noriega31@gmail.com>
2018-01-27 20:35:12 -0500

    Add FadeIn animation for level change

--- Scripts/BaseKnob.gd
+++ Scripts/BaseKnob.gd
@@ -5,9 +5,8 @@ var rotate_right = ""
 var degrees_per_frame = 0.25
 
 func _ready():
-	set_process(true)
-	
-	TimerGenerator.generate_timer(0.1, "_lock_on_pos", self).start()
+	TimerGenerator.generate_timer(0.05, "_lock_on_pos", self).start()
+	TimerGenerator.generate_timer(0.7, "_allow_controls", self).start()
 
 func _process(delta):
 	if Input.is_action_pressed(rotate_left):
@@ -19,5 +18,7 @@ func _lock_on_pos():
 	var target_pos = get_node("../Position2D").get_global_pos()
 	print(target_pos)
 	
-	print(rad2deg(atan2(get_global_pos().x - target_pos.x, get_global_pos().y - target_pos.y)))
 	set_rotd(rad2deg(atan2(get_global_pos().x - target_pos.x, get_global_pos().y - target_pos.y)))
+
+func _allow_controls():
+	set_process(true)
