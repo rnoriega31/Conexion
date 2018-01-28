ff6d09b72b077857c356f0d165be9ab96778fafd
Ricardo <r.noriega31@gmail.com>
2018-01-28 12:34:57 -0500

    Levels now reload on fail rather than sending you to the first level

---
+++ Scripts/Data.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var current_level = 0
+
+func _ready():
+	pass
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,7 +2,6 @@ extends Node
 
 var pos_ref
 var levels_array  = ["Level_1.tscn", "Level_2.tscn", "Level_3.tscn", "Level_4.tscn", "Level_5.tscn", "EndGame.tscn"]
-var cur_level = 0
 signal reset
 
 func _ready():
@@ -14,17 +13,17 @@ func _load_next_level():
 	_add_next_level()
 
 func _add_next_level():
-	cur_level += 1
+	Data.current_level += 1
 	
-	if cur_level < levels_array.size():
-		print("adding level " + levels_array[cur_level])
-		InstanceHelper.instance_scene(self, levels_array[cur_level])
+	if Data.current_level < levels_array.size():
+		print("adding level " + levels_array[Data.current_level])
+		InstanceHelper.instance_scene(self, levels_array[Data.current_level])
 
 func _add_same_level():
-	if cur_level < levels_array.size():
-		print("adding level " + levels_array[cur_level])
-		InstanceHelper.instance_scene(self, levels_array[cur_level])
+	if Data.current_level < levels_array.size():
+		print("reloading level " + levels_array[Data.current_level])
+		InstanceHelper.instance_scene(self, levels_array[Data.current_level])
 
 func _delete_current_level():
-	var node_name = levels_array[cur_level].left(levels_array[cur_level].find("."))
+	var node_name = levels_array[Data.current_level].left(levels_array[Data.current_level].find("."))
 	get_node(node_name).queue_free()
